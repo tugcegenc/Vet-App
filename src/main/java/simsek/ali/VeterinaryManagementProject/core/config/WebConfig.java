e046f671f1a9ca177e83a5da16b2f883687e6c55
Tuğçe Genç <tugcegenc@Tugce-MacBook-Air.local>
2024-06-07 16:37:11 +0300

    Updated application properties and Docker configuration

--- src/main/java/simsek/ali/VeterinaryManagementProject/core/config/WebConfig.java
+++ src/main/java/simsek/ali/VeterinaryManagementProject/core/config/WebConfig.java
@@ -1,17 +1,21 @@
-import org.springframework.context.annotation.Bean;
+package simsek.ali.VeterinaryManagementProject.core.config;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("*")
+    private String vetAppHost;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://illustrious-sprite-fc0fa6.netlify.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
