01291b730268f39af2ba2fdf88fdf4bd8500ad37
Tuğçe Genç <tugcegenc@Tugce-MacBook-Air.local>
2024-06-07 21:13:52 +0300

    Updated application properties and Docker configuration

--- src/main/java/simsek/ali/VeterinaryManagementProject/core/config/WebConfig.java
+++ src/main/java/simsek/ali/VeterinaryManagementProject/core/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://illustrious-sprite-fc0fa6.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
